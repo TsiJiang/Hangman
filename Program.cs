9bde3ece73ca8944182c1965a37b9a4518d94779
Xavier L <63250585+TsiJiang@users.noreply.github.com>
2020-07-09 14:43:11 -0600

    Modified original
    +pulled existing word dictionary into a enum
    +added a proper win condition and display for correctly guessed and missed characters
    +added display for previous guesses

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Drawing;
+using System.IO.Pipes;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Hangman
 {
@@ -11,81 +10,163 @@ namespace Hangman
         static void Main(string[] args)
         {
             Console.WriteLine("Let's Play Hangman");
-            string[] listwords = {"mongolian", "elephant", "canada", "morrocco", "construction",
-                                  "banana", "scrumptious", "serendipity", "california", "cavalier"};
             Random wordGen = new Random();
-            var index = wordGen.Next(0, 9);
-            string mysteryWord = listwords[index];
-            char[] guess = new char[mysteryWord.Length];
-            Console.WriteLine("Please enter a letter: ");
-            int guessCount = 0;
-            for (int i = 0; i < mysteryWord.Length; i++)
+            var index = wordGen.Next(0, Word.GetValues(typeof(ContentAlignment)).Length);
+            string mysteryWord = ((Word)index).ToString().ToLower();
+            string charGuesses = "";
+            string guess = "";
+            int badGuess = 0;
+            Console.ReadKey();
+            do
             {
-                guess[i] = '*';
-
-                while (true)
+                do
                 {
-                    for (int j = 0; j < mysteryWord.Length; j++)
+                    PrintBoard(badGuess);
+                    Console.WriteLine(PrintMysteryWord(mysteryWord,charGuesses));
+                    Console.WriteLine(charGuesses);
+                    Console.Write("\nPlease enter a letter: ");
+                    guess = Console.ReadLine();
+                    guess = guess.ToLower();
+                } while (char.IsLetter(guess.First()) == false);
+                charGuesses += $" {char.ToString(guess.First())}";
+                if (mysteryWord.Contains(guess) == false) 
+                {
+                    badGuess++;
+                }
+                guess = "";
+
+            } while (badGuess < 6 && PrintMysteryWord(mysteryWord, charGuesses).Contains('*'));
+            if (PrintMysteryWord(mysteryWord, charGuesses).Contains('*') == false)
+            {
+                Console.Clear();
+                Console.WriteLine("You Win!");
+                PrintBoard(badGuess);
+                Console.WriteLine(PrintMysteryWord(mysteryWord, charGuesses));
+                Console.WriteLine(charGuesses);
+            }
+            else 
+            {
+                PrintBoard(badGuess);
+                Console.WriteLine(PrintMysteryWord(mysteryWord, charGuesses));
+                Console.WriteLine(charGuesses);
+            }
+            Console.ReadLine();
+        } //End of Main
+        static void PrintBoard(int badGuess)
+        {
+            Console.Clear();
+            switch (badGuess)
+            {
+                case 0:
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    break;
+                case 1:
+                    Console.WriteLine();
+                    Console.WriteLine("   O  ");
+                    Console.WriteLine("      ");
+                    Console.WriteLine("      ");
+                    break;
+                case 2:
+                    Console.WriteLine();
+                    Console.WriteLine("   O  ");
+                    Console.WriteLine("   |  ");
+                    Console.WriteLine("      ");
+                    break;
+                case 3:
+                    Console.WriteLine();
+                    Console.WriteLine("   O  ");
+                    Console.WriteLine("   |  ");
+                    Console.WriteLine("  /   ");
+                    break;
+                case 4:
+                    Console.WriteLine();
+                    Console.WriteLine("   O  ");
+                    Console.WriteLine("   |  ");
+                    Console.WriteLine("  / \\ ");
+                    break;
+                case 5:
+                    Console.WriteLine();
+                    Console.WriteLine(" \\ O  ");
+                    Console.WriteLine("   |  ");
+                    Console.WriteLine("  / \\ ");
+                    break;
+                default:
+                    Console.WriteLine("You Lose!");
+                    Console.WriteLine(" \\ O /  ");
+                    Console.WriteLine("   |  ");
+                    Console.WriteLine("  / \\ ");
+                    break;
+            }
+        }
+        static string PrintMysteryWord(string mysteryWord, string guessString) 
+        {
+            string ans = "";
+            int i = 0;
+            foreach (char c in mysteryWord) 
+            {
+                foreach(char d in guessString) 
+                {
+                    if (c == d)
                     {
-                        char playerGuess = char.Parse(Console.ReadLine());
-                        if (playerGuess == mysteryWord[j])
-                        {
-                            guess[j] = playerGuess;
-                        }
-                        else if (playerGuess != mysteryWord[j])
-                        {
-                            if (guessCount == 1)
-                            {
-                                Console.WriteLine("  O  ");
-                            }
-
-                            else if (guessCount == 2)
-                            {
-                                Console.WriteLine("  O  ");
-                                Console.WriteLine("  |  ");
-                            }
-
-                            else if (guessCount == 3)
-                            {
-                                Console.WriteLine("  O  ");
-                                Console.WriteLine("  |  ");
-                                Console.WriteLine(" |   ");
-                            }
-
-                            else if (guessCount == 4)
-                            {
-                                Console.WriteLine("  O  ");
-                                Console.WriteLine("  |  ");
-                                Console.WriteLine(@" / \ ");
-                            }
-
-                            else if (guessCount == 5)
-                            {
-
-                                Console.WriteLine(@" \ O  ");
-                                Console.WriteLine("   |  ");
-                                Console.WriteLine(@"  / \ ");
-                            }
-
-                            else if (guessCount == 6)
-                            {
-                                Console.WriteLine("You Lose!");
-                                Console.WriteLine(@" \ O /  ");
-                                Console.WriteLine("   |  ");
-                                Console.WriteLine(@"  / \ ");
-                                break;
-                            }
-
-                        }
-                        guessCount++;
-                        Console.WriteLine(guess);
+                        ans += c;
+                        i++;
                     }
                 }
+                if (i < mysteryWord.Length && mysteryWord[i] == c) 
+                {
+                    ans += "*";
+                    i++;
+                }
             }
+            return ans;
         }
     }
 }
-    
+//if (guessCount == 1)
+//{
+//    Console.WriteLine("  O  ");
+//}
+
+//else if (guessCount == 2)
+//{
+//    Console.WriteLine("  O  ");
+//    Console.WriteLine("  |  ");
+//}
+
+//else if (guessCount == 3)
+//{
+//    Console.WriteLine("  O  ");
+//    Console.WriteLine("  |  ");
+//    Console.WriteLine(" |   ");
+//}
+
+//else if (guessCount == 4)
+//{
+//    Console.WriteLine("  O  ");
+//    Console.WriteLine("  |  ");
+//    Console.WriteLine(" / \\ ");
+//}
+
+//else if (guessCount == 5)
+//{
+
+//    Console.WriteLine(@" \ O  ");
+//    Console.WriteLine("   |  ");
+//    Console.WriteLine(@"  / \ ");
+//}
+
+//else if (guessCount == 6)
+//{
+//    Console.WriteLine("You Lose!");
+//    Console.WriteLine(@" \ O /  ");
+//    Console.WriteLine("   |  ");
+//    Console.WriteLine(@"  / \ ");
+//    break;
+//}
+
 
     //while (true)
     //{
---
+++ Word.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    enum Word
+    {
+        mongolian,
+        elephant,
+        canada,
+        morrocco,
+        construction,
+        banana,
+        scrumptious,
+        serendipity,
+        california,
+        cavalier
+    }
+}
